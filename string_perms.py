e772d9f91089fd72b1264c89a4358db79ead5752
Ellen Lawrence <ellen.t.lawrence@gmail.com>
2019-11-27 23:10:51 -0800

    string perms

---
+++ string_perms.py
@@ -0,0 +1,19 @@
+import unittest
+
+def find_string_perms(s, b):
+    '''Finds all the permutations of smaller string s inside bigger string b.'''
+
+
+
+
+class TestStringPerms(unittest.TestCase):
+    def test_find_string_perms(self):
+        '''Test that the string permutation function is returning the correct
+        number of permutations.'''
+
+        result = find_string_perms('abbc', 'cbabadcbbabbcbabaabccbabc')
+        self.assertEqual(result, 7)
+
+
+if __name__ == '__main__':
+    unittest.main()
