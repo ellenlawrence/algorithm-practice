4beb588853cda31d684054045eecd36e9928753b
Ellen Lawrence <ellen.t.lawrence@gmail.com>
2019-12-02 16:33:47 -0800

    sort by freq

--- sort_by_freq.py
+++ sort_by_freq.py
@@ -4,7 +4,20 @@ def sort_by_freq(lst):
     '''Sorts list of items by their frequency in the list and returns a list
     with the items listed by highest frequency to lowest.'''
 
+    
+    freqs = dict()
+    
+    for item in lst:
+        freqs[item] = freqs.get(item, 0) + 1
 
+    sorted_tuples = sorted(freqs.items(), key=lambda x: x[1], reverse=True)
+
+    list_by_freq = []
+
+    for tupl in sorted_tuples:
+        list_by_freq.append(tupl[0])
+
+    return list_by_freq
 
 
 class TestSortByFreq(unittest.TestCase):
