d9f7d2e0f771a01430019d626b5dea46512ba315
Ellen Lawrence <ellen.t.lawrence@gmail.com>
2020-01-08 00:16:58 -0800

    missing number

--- missing.py
+++ missing.py
@@ -12,6 +12,32 @@ def missing_number(nums, max_num):
     
     """
 
+    # solution 1
+    
+    # set_nums = set(nums)
+
+    # for num in range(1, max_num + 1):
+    #     if num not in set_nums:
+    #         return num
+
+
+    # solution 2
+    # 
+    # nums.sort()
+    # 
+    # for num in nums:
+    #     if nums[nums.index(num) + 1] and num == nums[nums.index(num) + 1] - 1:
+    #         continue
+    #     else:
+    #         return num + 1
+
+
+    # solution 3
+    
+    should_be = (max_num * (1 + max_num))/2
+    actually = sum(nums)
+    return int(should_be - actually)
+
 
 if __name__ == '__main__':
     import doctest
