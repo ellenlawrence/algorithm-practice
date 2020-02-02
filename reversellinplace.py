cddbf3105c9b172ac124532513e24feb482caf8f
Ellen Lawrence <ellen.t.lawrence@gmail.com>
2020-02-01 23:32:55 -0800

    reverse ll in place

--- reversellinplace.py
+++ reversellinplace.py
@@ -14,7 +14,7 @@ Recursive solution doctest:
     >>> ll2 = LinkedList(Node(1, Node(2, Node(3))))
     >>> ll2.as_string()
     '123'
-    >>> reverse_linked_list_in_place_rec(ll2)
+    >>> reverse_linked_list_in_place(ll2)
     >>> ll2.as_string()
     '321'
 """
@@ -56,6 +56,16 @@ class Node(object):
 # Iteration solution.
 def reverse_linked_list_in_place(lst):
     """Given linked list, reverse the nodes in this linked list in place."""
+
+    current = lst.head
+    prev = None
+
+    while current:
+        next = current.next
+        current.next = prev
+        prev = current
+        current = next
+    lst.head = prev        
 
 if __name__ == '__main__':
     import doctest
